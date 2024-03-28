92068bd41c4b3827c904880b721d320ba79536e5
juanpablobracco <64668332+juanpablobracco@users.noreply.github.com>
2024-03-27 22:08:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ document.getElementById('form').addEventListener('submit', (Event) => {
     const email = document.getElementById('email').value;
     const emailConfirmation = document.getElementById('email-confirmation');
     const error = document.getElementById('error');
-    console.log(email);
-
 
     if (email.trim() === '') {
         error.textContent = 'Please fill the field';
+        document.getElementById('email').style.backgroundcolor = 'hsl(4, 100%, 67%)';
+
     } else {
         error.textContent = ''; //Clear previous error
 
+        // REGEX to validate a proper email format
         let emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 
         if (!emailRegex.test(email)) {
             error.textContent = 'Wrong email format.';
         } else {
-            // If the email is ValidityState and not empty you can proceed with the form submission
+            // If the email is ValidityState and not empty you can proceed with the form submission and style change
             emailConfirmation.innerHTML = email;
             document.getElementById('container_signup_check').style.display = 'flex';
             document.getElementById('container').style.width = '500px';
@@ -26,10 +27,11 @@ document.getElementById('form').addEventListener('submit', (Event) => {
 
             document.getElementById('container_signup').style.display = 'none';
             document.getElementById('container_background').style.display = 'none';
-            
+
         }
     }
 })
 
+// Reload the site
 document.getElementById('continue').addEventListener('click', ()=> {
     location.reload() })
